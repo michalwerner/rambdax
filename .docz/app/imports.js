af1f24e0c12b05b2ea87ecb4037d162e9562c975
Dejan Toteff <deyan8284@gmail.com>
2019-05-29 21:09:23 +0300

    chore@start method component to show method tests and docs

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,26 +1,6 @@
 export const imports = {
-  'files/CodeStyle.md': () =>
+  'doc_files/index.mdx': () =>
     import(
-      /* webpackPrefetch: true, webpackChunkName: "files-code-style" */ 'files/CodeStyle.md'
-    ),
-  'files/isValid.md': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "files-is-valid" */ 'files/isValid.md'
-    ),
-  'files/examples/CreateDocs.md': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "files-examples-create-docs" */ 'files/examples/CreateDocs.md'
-    ),
-  'files/examples/PartialCurry.md': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "files-examples-partial-curry" */ 'files/examples/PartialCurry.md'
-    ),
-  'files/examples/PromiseAllChunks.md': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "files-examples-promise-all-chunks" */ 'files/examples/PromiseAllChunks.md'
-    ),
-  'files/examples/RandomImageName.md': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "files-examples-random-image-name" */ 'files/examples/RandomImageName.md'
+      /* webpackPrefetch: true, webpackChunkName: "doc-files-index" */ 'doc_files/index.mdx'
     ),
 }
---
+++ doc_files/Method.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Method({method}= {method: 'General'}){
+  return (
+    <div>{method}</div>
+  )
+}
