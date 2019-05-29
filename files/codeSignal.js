036fc61bbdd471d54336292a6e96d43692dffeb3
Dejan Toteff <deyan8284@gmail.com>
2019-05-29 20:52:01 +0300

    chore: code signal

---
+++ files/codeSignal.js
@@ -0,0 +1,19 @@
+
+function compose (...fns) {
+  return (...args) => {
+    const list = fns.slice()
+    if (list.length > 0) {
+      const fn = list.pop()
+      let result = fn(...args)
+      while (list.length > 0) {
+        result = list.pop()(result)
+      }
+
+      return result
+    }
+
+    return undefined
+  }
+}
+
+const pipe = (...fns) => compose(...fns.reverse())
